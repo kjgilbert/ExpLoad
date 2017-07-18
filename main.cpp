24fc268e7eb3458e05cc0544cb753ed41ac7c342
kjgilbert <kgilbert@zoology.ubc.ca>
2017-07-18 13:47:37 +0200

    the compile-able and interactive version with hardcoded parameters for testing in netbeans

--- main.cpp
+++ main.cpp
@@ -34,7 +34,7 @@ using namespace std;
 int main(int argc, char* argv[]) {
  
  // comment his out for debug, and the part in parenthesis of main above   
- ///*
+ /*
     if ( argc != 2 )
     {                   // argc should be length 2 for correct execution
                         //spot 0 is the program name when running it in command line
@@ -46,7 +46,7 @@ int main(int argc, char* argv[]) {
     
     string fn = argv[1]; //filename for parameter inputs
     cout << fn; 
-//*/
+*/
 
       
 //    fstream file;
@@ -76,7 +76,7 @@ int main(int argc, char* argv[]) {
 // find the path of the paramfile:
 // I think this one is the more useful because then I can put the outputs wherever the paramfile is stored
    // Get the last position of '/'
-///* 
+/* 
     std::string aux(argv[1]);       // comment this out for debug
 
     // get '/' or '\\' depending on unix/mac or windows.
@@ -90,7 +90,7 @@ int main(int argc, char* argv[]) {
     std::string path = aux.substr(0,pos+1);
     std::string name = aux.substr(pos+1);
     // show results
-//*/    
+*/    
     
     
     
@@ -199,7 +199,7 @@ int main(int argc, char* argv[]) {
 
     
     // comment all this out for debug with hardcoded params
-///* 
+/* 
     double par;
     vector<double> params;          // character array to hold all the inputs from paramfile
     
@@ -248,9 +248,9 @@ int main(int argc, char* argv[]) {
     {
         cout << "\n MISSING REQUIRED NUMBER OF PARAMETERS, EXITING SIMULATION.\n";
     }
-//*/ 
+*/ 
 
-/*   uncomment this to debug and have hardcoded parameters
+///*   uncomment this to debug and have hardcoded parameters
     m1 = 1;
     m2 = 500;
     starting_demes = 10;
@@ -275,7 +275,7 @@ int main(int argc, char* argv[]) {
 
     tot_demes = m1*m2;
     initial_colonized = starting_demes*m1;  
-*/
+//*/
     
    loci = 1000;     // right now number of loci has to be hard coded in 
     
